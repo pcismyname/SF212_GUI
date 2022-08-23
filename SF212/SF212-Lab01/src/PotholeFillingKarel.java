fbf39acef381ae1ca850580352ddbaf871374889
pcismynme <pcismyname007@gmail.com>
2022-08-23 23:50:59 +0700

    init

---
+++ SF212/SF212-Lab01/src/PotholeFillingKarel.java
@@ -0,0 +1,14 @@
+/*
+ * File: PotholeFillingKarel.java
+ * ------------------------------
+ * The PotholeFillingKarel class puts a beeper into a pothole
+ * on 2nd Avenue.
+ */
+
+import stanford.karel.*;
+
+public class PotholeFillingKarel extends Karel {
+
+	// You fill in this part
+	
+}
