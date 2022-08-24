398718a1fde6ad55a5677dabefecd9ce8e85c496
pcismynme <pcismyname007@gmail.com>
2022-08-24 09:35:21 +0700

    lab3

---
+++ SF212/SF212-Lab03/src/BanishWinter.java
@@ -0,0 +1,14 @@
+/*
+ * File: BanishWinter.java
+ * -----------------------
+ * The BanishWinter subclass gets Karel adorn a series
+ * of trees with a cluster of beeper leaves.
+ */
+
+import stanford.karel.*;
+
+public class BanishWinter extends SuperKarel {
+
+	// You fill in this part
+
+}
